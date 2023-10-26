2b9c1a03575a8b13cd1cabf22880f0779544aac9
nohsangwoo <nsgr12@naver.com>
2023-10-26 15:23:51 +0900

    feat: get youtube data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,150 +1,59 @@
-import RunScraperButton from "./components/RunScraperButton";
-const cheerio = require("cheerio");
-const puppeteer = require("puppeteer");
-let counter = 0;
+"use client";
 
-interface ownProps {
-    searchParams: any;
-}
+import { useState } from "react";
 
-export default function Home({ searchParams }: ownProps) {
-    // const searchParams = useSearchParams();
+interface ownProps {}
 
-    if (searchParams?.runScraperButton) {
-        console.log("searchParams", searchParams);
+export default function Home({}: ownProps) {
+    const [youtubeData, setYoutubeData] = useState<{
+        message?: string;
+        data?: { imageUrl: string; videoUrl: string; title: string }[];
+    }>();
+    const [getYoutubeLoading, setGetYoutubeLoading] = useState(false);
 
-        runScraper();
-    }
+    const onClickGetYoutubeData = async () => {
+        setGetYoutubeLoading(true);
+        const res = await fetch("http://localhost:3000/api/hello");
+        const data = await res.json();
+        console.log("data", data);
+        setYoutubeData(data);
+        setGetYoutubeLoading(false);
+        // return data;
+    };
+
+    console.log("youtubeData", youtubeData);
+    console.log("getYoutubeLoading", getYoutubeLoading);
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <RunScraperButton />
+            <button
+                className=" border border-black p-4 rounded-md"
+                onClick={onClickGetYoutubeData}
+            >
+                fetch from url
+            </button>
+            <div className=" h-[20px]"></div>
+            <div>
+                {getYoutubeLoading ? (
+                    <div>loading...</div>
+                ) : (
+                    <div>
+                        {youtubeData?.data?.map((el, i) => {
+                            return (
+                                <div
+                                    key={i}
+                                    className=" p-5 border border-blue-500 rounded-xl mb-4"
+                                >
+                                    <div>title: {el.title}</div>
+                                    <div>
+                                        imageUrl: <img src={el.imageUrl} />
+                                    </div>
+                                    <div>videoUrl: {el.videoUrl}</div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
         </main>
     );
 }
-
-const runScraper = async () => {
-    // launch puppeteer browser
-
-    const browser = await puppeteer.launch({
-        headless: false,
-        // defaultViewport: null,
-        // args: ["--start-maximized"],
-    });
-
-    const page = await browser.newPage();
-
-    // set browser viewport to 1300x600
-    await page.setViewport({ width: 1300, height: 600 });
-
-    // go to url
-
-    const endpoint = "http://books.toscrape.com/";
-
-    await page.goto(endpoint, { waitUntil: "domcontentloaded" });
-
-    // wait 3 seconds
-    wait(3000);
-
-    // click category
-    await clickCategory(page);
-
-    // wait 3 seconds
-    wait(3000);
-
-    // extract data
-    await scrapeData(page);
-};
-
-// wait function
-const wait = (ms: number) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
-const clickCategory = async (page: any) => {
-    const catAttr = "catalogue/category/books/mystery_3/index.html";
-    if (!(await page.$(`a[href="${catAttr}"]`))) {
-        counter++;
-        if (counter < 3) {
-            console.log(
-                `can\'t find category selector... Running retry number${counter}`
-            );
-            await wait(2000);
-            await clickCategory(page);
-        } else {
-            console.log(`Unable to find category selector... Moving on.`);
-            counter = 0;
-        }
-        return;
-    }
-    counter = 0;
-    await page.click(`a[href="${catAttr}"]`);
-};
-
-// scrape data
-const scrapeData = async (page: any) => {
-    const $ = cheerio.load(await page.content());
-
-    if (!(await page.$("ol.row li"))) {
-        counter++;
-        console.log(
-            `can\'t find category selector... Running retry number${counter}`
-        );
-        if (counter < 3) {
-            // wait 2 seconds and try again
-            await wait(2000);
-            await scrapeData(page);
-        } else {
-            console.log(`Unable to find category selector... Moving on.`);
-            counter = 0;
-        }
-    }
-
-    // get li tags
-    const liTags = $("ol.row li");
-
-    let booksArr: {
-        imageUrl: string;
-        starRating: string;
-        title: string;
-        price: string;
-        availability: string;
-    }[] = [];
-    // extract data for  each book
-    const baseUrl = "http://books.toscrape.com/";
-    liTags.each((i: any, el: any) => {
-        // img태그안의 scr속성의 내용을 가져옴.
-        let imageUrl = $(el).find("img").attr("src");
-        imageUrl = imageUrl.replaceAll("../", "").trim();
-        imageUrl = baseUrl + imageUrl;
-
-        let starRating = $(el).find("p.star-rating").attr("class");
-        starRating = starRating.replaceAll("star-rating", "").trim();
-
-        // h3태그 하위의 text를 가져옴.
-        const title = $(el).find("h3").text().trim();
-
-        // title을 가져오는 또다른 방법
-        // h3태그안의 a태그안의 title속성의 내용을 가져옴.
-        // const title = $(el).find("h3 a").attr("title").trim();
-
-        // p태그의 price_color클래스의 text를 가져옴.
-        const price = $(el).find("p.price_color").text().trim();
-
-        const availability = $(el).find("p.instock.availability").text().trim();
-        // 이런형식으로도 가져올 수 있음.
-        /*     const availability = $(el)
-            .find('p[class="instock availability"]')
-            .text()
-            .trim(); */
-
-        const book = {
-            imageUrl,
-            starRating,
-            title,
-            price,
-            availability,
-        };
-        booksArr.push(book);
-    });
-    console.log("booksArr: ", booksArr);
-};
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,12 +1,108 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 type ResponseData = {
-    message: string;
+    message?: string;
+    data?: {
+        imageUrl: string;
+        videoUrl: string;
+        title: string;
+    }[];
 };
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseData>
 ) {
-    res.status(200).json({ message: "Hello from Next.js!asdfasd" });
+    let counter = 0;
+    let yt_data: {
+        imageUrl: string;
+        videoUrl: string;
+        title: string;
+    }[] = [];
+
+    const runScraper = async () => {
+        // launch puppeteer browser
+
+        const browser = await puppeteer.launch({
+            headless: false,
+            // defaultViewport: null,
+            // args: ["--start-maximized"],
+        });
+
+        const page = await browser.newPage();
+
+        // set browser viewport to 1800x1000
+        await page.setViewport({ width: 1800, height: 3000 });
+
+        // go to url
+
+        const endpoint =
+            "https://www.youtube.com/feed/trending?bp=4gINGgt5dG1hX2NoYXJ0cw%3D%3D";
+
+        await page.goto(endpoint, { waitUntil: "domcontentloaded" });
+
+        // wait 3 seconds
+        wait(4000);
+
+        // extract data
+        await scrapeData(page);
+    };
+
+    // wait function
+    const wait = (ms: number) => {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    };
+
+    // scrape data
+    const scrapeData = async (page: any) => {
+        const $ = cheerio.load(await page.content());
+
+        if (!(await page.$("#grid-container ytd-video-renderer"))) {
+            counter++;
+            console.log(
+                `can\'t find category selector... Running retry number${counter}`
+            );
+            if (counter < 3) {
+                // wait 2 seconds and try again
+                await wait(2000);
+                await scrapeData(page);
+            } else {
+                console.log(`Unable to find category selector... Moving on.`);
+                counter = 0;
+            }
+        }
+
+        const yt_list = await $("#grid-container ytd-video-renderer");
+        yt_list.each((i: any, el: any) => {
+            if (i >= 20) {
+                return;
+            }
+            // img태그안의 scr속성의 내용을 가져옴.
+            const imageUrl = $(el).find("img").attr("src");
+            const baseVideoUrl = "https://www.youtube.com";
+            let videoUrl = $(el).find("#thumbnail").attr("href").trim();
+            videoUrl = baseVideoUrl + videoUrl;
+
+            const title = $(el).find("#title-wrapper").text().trim();
+            const yt = {
+                imageUrl,
+                videoUrl,
+                title,
+            };
+            yt_data.push(yt);
+        });
+
+        console.log("yt_data inside: ", yt_data);
+        res.status(200).json({ message: "Hello from Next.js!", data: yt_data });
+    };
+
+    await runScraper(); // runScraper 함수가 완료될 때까지 기다림
+
+    console.log("yt_data outside: ", yt_data);
+
+    res.status(200).json({
+        message: "Hello from Next.js!asdfasd2222",
+        data: yt_data,
+    });
 }
